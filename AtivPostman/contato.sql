b4533743417e8ced8b2275dce4a30faa7b99f8c4
RaquelBasto <88353438+RaquelBasto@users.noreply.github.com>
2022-04-07 18:19:15 -0300

    Add files via upload
    Finalizado

---
+++ AtivPostman/contato.sql
@@ -0,0 +1,25 @@
+create database dbContatos;
+use dbContatos;
+
+create table tbContatos(
+    nome varchar(100),
+    email varchar(100)
+);
+
+INSERT INTO tbContatos(nome,email)
+VALUES("Maria","maria@hotmail.com"),
+("Antonio","antonio@hotmail.com"),
+("Jose","jose@hotmail.com"),
+("Amarildo","amarildo@hotmail.com"),
+("Noemi","noemi@hotmail.com"),
+("Dalva","dalva@hotmail.com"),
+("Lorivaldo","lorivaldo@hotmail.com"),
+("Alice","alice@hotmail.com"),
+("Nicole","nicole@hotmail.com,"),
+("Vicente","vicente@hotmail.com"),
+("Emanuel","emanuel@hotmail.com"),
+("Gabriela","gabriela@hotmail.com");
+
+select * from tbContatos;
+
+
